c02f575f94dd96751f073dd46d014e376e5f7d77
soyeolesafm <soyelesafm@hotmail.com>
2020-11-11 20:43:37 -0600

    queryselecting buttons

--- app.js
+++ app.js
@@ -1,99 +1,129 @@
-var equalButton = document.getElementById('calculate');
+// var equalButton = document.getElementById('calculate');
 
 
-var addNumber = document.getElementById('submit');
+// var addNumber = document.getElementById('submit');
 
-const numberButtons = document.querySelectorAll('.number')
+// *SELECTING BUTTONS*
 
-numberButtons.addEventListener("click", forEach.updateDisplay);
+const numberButtons = document.querySelectorAll(`.number`).length;
+const operationButtons = document.querySelectorAll(`.operation`).length;
+const allClear = document.querySelector(`#all-clear`)
+const deleteButton = document.querySelector(`#delete`)
+const equalButton = document.querySelector(`#equal`)
 
-function updateDisplay{
-    alert('got clicked')
+equalButton.addEventListener(`click`, function() {
+    alert('equal')
+})
+
+
+deleteButton.addEventListener(`click`, function() {
+    alert (`delete`)
+})
+
+allClear.addEventListener(`click`, function (){
+    alert(`all clear`)
+})
+
+for (var i = 0; i < numberButtons; i++) {
+    document.querySelectorAll(`.number`)[i].addEventListener(`click`, function (){
+        alert('number');
+    }) 
+    
+}
+
+for (var i = 0; i < operationButtons; i++) {
+    document.querySelectorAll(`.operation`)[i].addEventListener(`click`, function (){
+        alert('operation');
+    }) 
+    
 }
 
 
 
 
-var arr = []
 
 
-function addition() {
-    var val = 0
-    for (var i = 0; i< arr.length; i++){
-        val += arr[i]
+
+// var arr = []
+
+
+// function addition() {
+//     var val = 0
+//     for (var i = 0; i< arr.length; i++){
+//         val += arr[i]
         
-    }
-    document.getElementById('result').innerHTML = val;
+//     }
+//     document.getElementById('result').innerHTML = val;
     
-}
-function substraction() {
-    var val = (Math.max.apply(Math, arr)) * 2; 
-    for (var i = 0; i< arr.length; i++){
-        val -= arr[i];
+// }
+// function substraction() {
+//     var val = (Math.max.apply(Math, arr)) * 2; 
+//     for (var i = 0; i< arr.length; i++){
+//         val -= arr[i];
         
-    }
+//     }
     
-    document.getElementById('result').innerHTML = val
-}
-function division() {
-    var a = arr[0];
-    var b = arr[1];
-    var val = a / b;
+//     document.getElementById('result').innerHTML = val
+// }
+// function division() {
+//     var a = arr[0];
+//     var b = arr[1];
+//     var val = a / b;
     
-    document.getElementById('result').innerHTML = val
-}
-function multiplication() {
-    var val = 1;
-    for (var i = 0; i< arr.length; i++){
-        val *= arr[i];
-    }
-    document.getElementById('result').innerHTML = val
-}
+//     document.getElementById('result').innerHTML = val
+// }
+// function multiplication() {
+//     var val = 1;
+//     for (var i = 0; i< arr.length; i++){
+//         val *= arr[i];
+//     }
+//     document.getElementById('result').innerHTML = val
+// }
 
-function calculate(op, arr) {
+// function calculate(op, arr) {
 
-    var op =document.getElementById('operator').value
+//     var op =document.getElementById('operator').value
 
-    switch (op) {
-        case '+':
-            return addition(arr);
-        case '-':
-            return substraction(arr);
-        case '*':
-            return multiplication(arr);
-        case '/':
-            return division(arr);
+//     switch (op) {
+//         case '+':
+//             return addition(arr);
+//         case '-':
+//             return substraction(arr);
+//         case '*':
+//             return multiplication(arr);
+//         case '/':
+//             return division(arr);
             
-            alert(val);
-    } 
+//             alert(val);
+//     } 
     
 
  
-}
+// }
 
 
 
-function subNum() {
+// function subNum() {
     
-    var num = parseFloat(document.getElementById('vnum').value  );
+//     var num = parseFloat(document.getElementById('vnum').value  );
 
     
     
-    if(isNaN(num) === true) return
+//     if(isNaN(num) === true) return
 
-    arr.push(num)
+//     arr.push(num)
 
-    var valN = ''
+//     var valN = ''
 
-    for(i = 0; i < arr.length; i++){
+//     for(i = 0; i < arr.length; i++){
 
-        valN = valN + arr[i] + ", ";
-    }
-    document.getElementById('dispN').innerHTML = valN;
+//         valN = valN + arr[i] + ", ";
+//     }
+//     document.getElementById('dispN').innerHTML = valN;
 
-    parseFloat(document.getElementById('vnum').value = '');
-}
+//     parseFloat(document.getElementById('vnum').value = '');
+// }
 
-equalButton.addEventListener('click', calculate)
 
-addNumber.addEventListener('click', subNum);
+
+// addNumber.addEventListener('click', subNum);
